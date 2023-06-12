40832b05f1923848e89b3d566a9004b47dd8d30b
Minhee Lee <91667853+leeminhee119@users.noreply.github.com>
2023-06-12 14:17:52 +0900

    [level 2] Title: 큰 수 만들기, Time: 0.05 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.js
+++ 프로그래머스/lv2/42883. 큰 수 만들기/큰 수 만들기.js
@@ -1,4 +1,18 @@
 function solution(number, k) {
-    var answer = '';
-    return answer;
+    let cntK = 0;
+    const stack = [];
+    for (const item of number) {
+        while (cntK < k && stack[stack.length - 1] < item) {
+            stack.pop();
+            cntK ++;
+        }
+        stack.push(item);
+    }
+    
+    // 987631에 대한 예외처리 (pop이 k번만큼 다 안 되는 경우)
+    while (cntK < k) {
+        stack.pop();
+        cntK ++;
+    }
+    return stack.join('');
 }
