3e71061d4a41ae28e2c8218734663a3fcedb288b
Minhee Lee <91667853+leeminhee119@users.noreply.github.com>
2023-07-11 14:15:08 +0900

    [level 0] Title: 안전지대, Time: 0.24 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120866. 안전지대/안전지대.js
+++ 프로그래머스/lv0/120866. 안전지대/안전지대.js
@@ -1,25 +1,4 @@
 function solution(board) {
-    const dangerSpots = [];
-    const directions = [[-1, 1], [0, 1], [1, 1], [-1, 0], [1, 0], [-1, -1], [0, -1], [1, -1]]
-    const size = board.length;
-    let result = size * size;
-    
-    for (let row = 0; row < board.length; row++ ) {
-        for (let col = 0; col < board[row].length; col++) {
-            const spot = board[row][col];
-            if (spot === 0) continue;
-            else if (spot === 1) {
-                result--;
-                directions.forEach(direction => {
-                    const nrow = row + direction[0]
-                    const ncol = col + direction[1]
-                    if (nrow < size && nrow >= 0 && ncol < size && ncol >= 0 && !board[nrow][ncol]) {
-                        board[nrow][ncol] = 2;
-                        result--;
-                    }
-                })
-            }
-        }
-    }
-    return result;
+    var answer = 0;
+    return answer;
 }
