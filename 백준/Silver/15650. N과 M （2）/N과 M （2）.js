0cf5ab985b439fb33ffa01469fb492eda3ae4d78
Minhee Lee <91667853+leeminhee119@users.noreply.github.com>
2024-01-19 14:50:33 +0900

    [Silver III] Title: N과 M (2), Time: 148 ms, Memory: 9460 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.js
@@ -0,0 +1,37 @@
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let input = [];
+
+readline
+  .on("line", function (line) {
+    input.push(line);
+  })
+  .on("close", function () {
+    solution(input);
+    process.exit();
+  });
+
+function solution(input) {
+  const [N, M] = input[0].split(" ").map(Number);
+  const sequences = [];
+  function backtrack(seq) {
+    if (seq.length === M) {
+      sequences.push([...seq]);
+      return;
+    }
+    for (let i = 1; i <= N; i++) {
+      if (seq.includes(i) || seq[seq.length - 1] >= i) {
+        continue;
+      }
+      seq.push(i);
+      backtrack(seq);
+      seq.pop();
+    }
+  }
+
+  backtrack([]);
+  console.log(sequences.map((seq) => seq.join(" ")).join("\n"));
+}
