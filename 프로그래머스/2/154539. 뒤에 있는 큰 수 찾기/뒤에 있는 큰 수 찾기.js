5cf7faa53c41f22042f269bc3d99969f8d3e2278
Minhee Lee <91667853+leeminhee119@users.noreply.github.com>
2024-04-16 13:59:24 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 137.38 ms, Memory: 149 MB -BaekjoonHub

--- 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.js
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.js
@@ -1,12 +1,11 @@
 function solution(numbers) {
-    const result = Array.from({length: numbers.length}, () => -1);
+    const answer = Array.from({length: numbers.length}, () => -1);
     const stack = [];
     for (let i = 0; i < numbers.length; i++) {
-        while (stack.length && numbers[stack[stack.length - 1]] < numbers[i]) {
-            result[stack.pop()] = numbers[i];
+        while (numbers[stack[stack.length - 1]] < numbers[i]) {
+            answer[stack.pop()] = numbers[i];
         }
         stack.push(i);
     }
-    
-    return result;
+    return answer;
 }
