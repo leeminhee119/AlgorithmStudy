5b9c0e6ef850f3d4b32e41669d8cae4328a88347
Minhee Lee <91667853+leeminhee119@users.noreply.github.com>
2024-04-17 15:39:26 +0900

    [level 2] Title: 숫자 변환하기, Time: 120.22 ms, Memory: 55.4 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.js
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.js
@@ -22,31 +22,30 @@ class Queue {
   }
 }
 
-// BFS, DFS로 풀다가 시간초과가 나온다면 DP를 고려해보자.
 function solution(x, y, n) {
-    const dp = Array.from({length: y * 3}, () => false);
-    const queue = new Queue();
-    queue.enqueue([x, 0]);
-    dp[x] = true;
-    while (queue.size()) {
-        const [cur, cnt] = queue.dequeue();
-        if (cur === y) {
-            return cnt;
-        } else if (cur > y) {
-            continue;
-        }
-        if (!dp[cur + n]) {
-            queue.enqueue([cur + n, cnt + 1]);
-            dp[cur + n] = true;
-        }
-        if (!dp[cur * 2]) {
-            queue.enqueue([cur * 2, cnt + 1]);
-            dp[cur * 2] = true;
-        }
-        if (!dp[cur * 3]) {
-            queue.enqueue([cur * 3, cnt + 1]);
-            dp[cur * 3] = true;
+    const calculations = [(x) => x + n, (x) => 2 * x, (x) => 3 * x];
+    const visited = Array.from({length: y}, () => false);
+    let answer = 1000000000;
+    let flag = false;
+    function bfs(number) {
+        const queue = new Queue();
+        queue.enqueue([number, 0]);
+        while (queue.size()) {
+            const [curNum, cnt] = queue.dequeue();
+            if (curNum === y) {
+                flag = true;
+                answer = Math.min(answer, cnt);
+                continue;
+            }
+            for (let i = 0; i < calculations.length; i++) {
+                const nextNum = calculations[i](curNum);
+                if (nextNum > y) continue;
+                if (visited[nextNum]) continue;
+                queue.enqueue([nextNum, cnt + 1]);
+                visited[nextNum] = true;
+            }
         }
     }
-    return -1;
+    bfs(x);
+    return flag ? answer : -1;
 }
