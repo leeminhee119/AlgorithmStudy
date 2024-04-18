0c6e19aac486f5a0598cd90f726fe5d561542218
Minhee Lee <91667853+leeminhee119@users.noreply.github.com>
2024-04-18 12:52:35 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 17.21 ms, Memory: 47.4 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.js
@@ -1,28 +1,56 @@
+class Queue {
+    constructor(arr) {
+        this.queue = arr;
+        this.front = 0;
+        this.rear = arr.length;
+    }
+    enqueue(value) {
+        this.queue[this.rear++] = value;
+    }
+    dequeue() {
+        if (this.rear <= this.front) return;
+        const target = this.queue[this.front];
+        this.front++;
+        return target;
+    }
+    size() {
+        return this.rear - this.front;
+    }
+}
+
 function solution(queue1, queue2) {
-    let queue1Sum = queue1.reduce((acc, cur) => acc + cur, 0);
-    let queue2Sum = queue2.reduce((acc, cur) => acc + cur, 0);
-    const limit = queue1.length + queue2.length;
-    let queue1Idx = 0;
-    let queue2Idx = 0;
-    let answer = -1;
-    
-    while (queue1Idx < limit && queue2Idx < limit) {
-        if (queue1Sum === queue2Sum) {
-            answer = queue1Idx + queue2Idx;
+    // const q1 = new Queue(queue1);
+    // const q2 = new Queue(queue2);
+    const [q1Len, q2Len] = [queue1.length, queue2.length];
+    let q1Sum = queue1.reduce((acc, cur) => acc + cur, 0);
+    let q2Sum = queue2.reduce((acc, cur) => acc + cur, 0);
+    let q1Cnt = 0;
+    let q2Cnt = 0;
+    let q1First = 0;
+    let q2First = 0;
+    let flag = false;
+    while (q1Cnt < q1Len || q2Cnt < q2Len) {
+        if (q1Sum === q2Sum) {
+            flag = true;
             break;
         }
-        if (queue1Sum > queue2Sum) {
-            queue1Sum -= queue1[queue1Idx];
-            queue2Sum += queue1[queue1Idx];
-            queue2.push(queue1[queue1Idx]);
-            queue1Idx ++;
+        if (q1Sum > q2Sum) {
+            // const target = q1.dequeue();
+            // q2.enqueue(target);
+            const target = queue1[q1First++];
+            queue2.push(target);
+            q2Sum += target;
+            q1Sum -= target;
+            q1Cnt++;            
         } else {
-            queue2Sum -= queue2[queue2Idx];
-            queue1Sum += queue2[queue2Idx];
-            queue1.push(queue2[queue2Idx]);
-            queue2Idx ++;
+            // const target = q2.dequeue();
+            // q1.enqueue(target);
+            const target = queue2[q2First++];
+            queue1.push(target);
+            q1Sum += target;
+            q2Sum -= target;
+            q2Cnt++;       
         }
     }
-    
-    return answer;
+    return flag ? q1Cnt + q2Cnt : -1;
 }
