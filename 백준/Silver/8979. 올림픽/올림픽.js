9d2c314147236b0ecfc195420f3032c7fd06cd23
Minhee Lee <91667853+leeminhee119@users.noreply.github.com>
2024-07-06 10:02:25 +0900

    [Silver V] Title: 올림픽, Time: 144 ms, Memory: 9456 KB, Score: 20 point -BaekjoonHub

---
+++ 백준/Silver/8979. 올림픽/올림픽.js
@@ -0,0 +1,46 @@
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+const lines = [];
+
+readline
+  .on("line", function (line) {
+    lines.push(line);
+  })
+  .on("close", function () {
+    solution(lines);
+    process.exit();
+  });
+
+function solution(lines) {
+  const [N, K] = lines[0].split(" ").map(Number);
+  const infos = lines.slice(1).map((line) => line.split(" ").map(Number));
+  infos.sort((a, b) => b[3] - a[3]);
+  infos.sort((a, b) => b[2] - a[2]);
+  infos.sort((a, b) => b[1] - a[1]);
+  const index = infos.findIndex(([countryNo, , ,]) => countryNo === K);
+  let countEqual = 0;
+  for (let i = index - 1; i >= 0; i--) {
+    if (
+      infos[i][1] !== infos[index][1] ||
+      infos[i][2] !== infos[index][2] ||
+      infos[i][3] !== infos[index][3]
+    ) {
+      break;
+    }
+    countEqual++;
+  }
+  for (let i = index + 1; i < infos.length; i++) {
+    if (
+      infos[i][1] !== infos[index][1] ||
+      infos[i][2] !== infos[index][2] ||
+      infos[i][3] !== infos[index][3]
+    ) {
+      break;
+    }
+    countEqual++;
+  }
+  console.log(index + 1 - countEqual);
+}
