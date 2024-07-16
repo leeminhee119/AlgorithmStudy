e197c4b58983d1204978eee88547054e80150677
Minhee Lee <91667853+leeminhee119@users.noreply.github.com>
2024-07-16 10:43:37 +0900

    [Silver III] Title: 주유소, Time: 376 ms, Memory: 63836 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.js
+++ 백준/Silver/13305. 주유소/주유소.js
@@ -3,35 +3,41 @@ const readline = require("readline").createInterface({
   output: process.stdout,
 });
 
-let input = [];
+const lines = [];
 
 readline
   .on("line", function (line) {
-    input.push(line);
+    lines.push(line);
   })
   .on("close", function () {
-    solution(input);
+    solution(lines);
     process.exit();
   });
 
-function solution(input) {
-  const N = parseInt(input[0]);
-  const distances = input[1].split(" ").map(Number);
-  const prices = input[2].split(" ").map(Number);
-  let answer = 0;
-  let i = 0;
-  while (i < prices.length - 1) {
-    let nextCheapIdx = i + 1;
-    while (
-      nextCheapIdx < prices.length - 1 &&
-      prices[nextCheapIdx] > prices[i]
-    ) {
-      nextCheapIdx++;
+function solution(lines) {
+  const N = +lines[0];
+  const distances = lines[1].split(" ").map(BigInt);
+  const prices = lines[2].split(" ").map(BigInt);
+
+  // 1km당 1리터가 필요
+  // 최소 distance만큼의 기름이 필요
+
+  function getMinCost(curIdx, curCost) {
+    let nextIdx = curIdx + 1n;
+    let nextDistance = distances[curIdx];
+    // (더 싸거나 같은 주유소 이전까지의 거리), (현재 남아있는 기름)을 비교하여 다음 목적지 구하기
+    while (nextIdx < N - 1 && prices[nextIdx] > prices[curIdx]) {
+      nextDistance += distances[nextIdx++];
     }
-    answer +=
-      prices[i] *
-      distances.slice(i, nextCheapIdx).reduce((acc, cur) => acc + cur, 0);
-    i = nextCheapIdx;
+
+    // 더 싼 주유소가 앞에 없다면 남은 거리까지 부족한 만큼 전부 충전
+    if (nextIdx >= N - 1) {
+      return curCost + nextDistance * prices[curIdx];
+    }
+
+    // 더 싼 주유소로 '바로' 이동, 계속 비교
+    return getMinCost(nextIdx, curCost + nextDistance * prices[curIdx]);
   }
-  console.log(answer);
+
+  console.log(getMinCost(0n, 0n).toString());
 }
