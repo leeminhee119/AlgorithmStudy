e9e695e019704413ed72deb58817b5d0dee80654
Minhee Lee <91667853+leeminhee119@users.noreply.github.com>
2024-09-03 19:54:01 +0900

    [level 2] Title: 호텔 대실, Time: 9.56 ms, Memory: 36.4 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/README.md
+++ 프로그래머스/2/155651. 호텔 대실/README.md
@@ -1,10 +1,10 @@
 # [level 2] 호텔 대실 - 155651 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/155651#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/155651) 
 
 ### 성능 요약
 
-메모리: 37.7 MB, 시간: 12.89 ms
+메모리: 36.4 MB, 시간: 9.56 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 14일 02:00:03
+2024년 09월 03일 19:53:57
 
 ### 문제 설명
 
--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.js
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.js
@@ -1,21 +1,39 @@
 function solution(book_time) {
-    function getMinutes(time) {
-        const [h, m] = time.split(':').map(Number);
+    function getMinutes(timeString) {
+        const [h, m] = timeString.split(':').map(Number);
         return h * 60 + m;
     }
-    const sortedBookTime = book_time.map(([s, e]) => [getMinutes(s), getMinutes(e)]).sort((a, b) => a[1] - b[1]);
-    const sortedStartTime = book_time.map(([s, e]) => [getMinutes(s), getMinutes(e)]).sort((a, b) => a[0] - b[0]);
-    const LEN = sortedBookTime.length;
-    const rooms = [];
+    // 퇴실 후 10분까지 포함, 단위: 분
+    const occupied_time = book_time.map(([start, end]) => [getMinutes(start), getMinutes(end) + 10]);
     
-    for (let i = 0; i < LEN; i++) {
-        const [cS, cE] = sortedStartTime[i];
-        const roomIndex = rooms.findIndex((roomEndTime) => cS - roomEndTime >= 10);
-        if (roomIndex > -1) {
-            rooms[roomIndex] = cE;
+    // 방의 개수가 최소여야 함
+    // 
+    // 모든 방을 순회하며 종료 시각이 시작 시각보다 이전이면 그 방을 갱신 (마지막 종료시간을 담은 일차원 배열)
+
+    // 시간복잡도 O(N^2)
+    
+    occupied_time.sort((a, b) => a[0] - b[0]);
+    
+    const rooms = [];
+    occupied_time.forEach(([start, end]) => {
+        // let isJoined = false;
+        // for (let i = 0; i < rooms.length; i++) {
+        //     if (rooms[i][1] <= start) {
+        //         rooms[i] = [start, end];
+        //         isJoined = true;
+        //     }
+        // }
+        // if (!isJoined) {
+        //     rooms.push([start, end]);
+        // }
+        // rooms.sort((a, b) => b[1] - a[1]);
+        const idx = rooms.findIndex((roomEnd) => roomEnd <= start);
+        if (idx < 0) {
+            rooms.push(end);
         } else {
-            rooms.push(cE);
+            rooms[idx] = end;
         }
-    }
+    })
+    
     return rooms.length;
 }
