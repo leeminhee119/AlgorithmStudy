758670100184ff5127e1923345cb0af406d6ccbb
Minhee Lee <91667853+leeminhee119@users.noreply.github.com>
2024-09-07 10:18:01 +0900

    [level 2] Title: 시소 짝꿍, Time: 68.60 ms, Memory: 42.5 MB -BaekjoonHub

--- 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.js
+++ 프로그래머스/2/152996. 시소 짝꿍/시소 짝꿍.js
@@ -1,14 +1,27 @@
 function solution(weights) {
-    const ratios = [1, 2, 3 / 2, 4 / 3];
-    const valueMap = new Map();
+    // 곱이 양쪽이 같으면 짝꿍
+    
+    weights.sort((a, b) => b - a);
+    // [100, 100, 180, 270, 360]
+    // [360, 270, 180, 100, 100]
+    
+    const values = [1, 2, 3 / 2, 4 / 3];
+    
+    // Map에 가중치를 곱한 무게를 몇 개 있는지 누적해준다.
+    // 가중치를 곱한 weights들을 순회하면서 Map에 짝꿍이 있는지 확인한다.
+    
+    const countMap = new Map();
     let answer = 0;
-    weights.sort((a, b) => b - a).forEach((weight) => {
-        ratios.map((ratio) => weight * ratio).forEach((value) => {
-            if (valueMap.has(value)) {
-                answer += valueMap.get(value);
+    
+    weights.forEach((weight) => {
+        values.map((v) => v * weight).forEach((vWeight) => {
+            if (countMap.has(vWeight)) {
+                answer += countMap.get(vWeight);
             }
         })
-        valueMap.set(weight, valueMap.get(weight) ? valueMap.get(weight) + 1 : 1);
+        const cntWeight = countMap.get(weight);
+        countMap.set(weight, cntWeight ? cntWeight + 1 : 1);
     })
+
     return answer;
 }
