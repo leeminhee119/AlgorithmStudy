d125cc871c49b08a0f6302cc770f9e5b58f8f064
Minhee Lee <91667853+leeminhee119@users.noreply.github.com>
2024-10-11 22:07:55 +0900

    [Silver IV] Title: 설탕 배달, Time: 112 ms, Memory: 9500 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.js
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.js
@@ -1,34 +1,36 @@
-const fs = require("fs");
-
 const readline = require("readline").createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-let input = [];
+const lines = [];
 
 readline
   .on("line", function (line) {
-    input.push(line);
+    lines.push(line);
   })
   .on("close", function () {
-    solution(input);
+    solution(lines);
     process.exit();
   });
 
-function solution(input) {
-  const N = Number(input[0]);
-  const dp = Array.from({ length: 5000 + 1 }, () => -1);
+function solution(lines) {
+  const N = +lines[0];
+  // 5킬로그램을 더 많이 가져가야 유리
+  // dp[N] = dp[N - 5] + 1;
+  const dp = Array.from({ length: N + 1 }, () => -1);
   dp[3] = 1;
   dp[5] = 1;
+
   for (let i = 6; i <= N; i++) {
-    if (dp[i - 5] > -1 && dp[i - 3] > -1) {
-      dp[i] = Math.min(dp[i - 3], dp[i - 5]) + 1;
-    } else if (dp[i - 5] > -1) {
-      dp[i] = dp[i - 5] + 1;
-    } else if (dp[i - 3] > -1) {
+    if (dp[i - 5] === -1) {
+      if (dp[i - 3] === -1) {
+        continue;
+      }
       dp[i] = dp[i - 3] + 1;
+      continue;
     }
+    dp[i] = dp[i - 5] + 1;
   }
   console.log(dp[N]);
 }
