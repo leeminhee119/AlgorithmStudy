11981722ad32dcc532074921888873b63a0a9308
Minhee Lee <91667853+leeminhee119@users.noreply.github.com>
2024-10-11 20:09:34 +0900

    [level 4] Title: FrontEnd 개발자 찾기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
+++ 프로그래머스/4/276035. FrontEnd 개발자 찾기/FrontEnd 개발자 찾기.sql
@@ -1,8 +1,9 @@
 -- 코드를 작성해주세요
+
 SELECT d.id, d.email, d.first_name, d.last_name
 FROM developers as d
-JOIN skillcodes as s
-ON (d.skill_code & s.code) > 0
-WHERE s.category = 'Front End'
+JOIN skillcodes as sc
+ON (d.skill_code & sc.code) > 0
+WHERE sc.category = 'Front End'
 GROUP BY d.id, d.email, d.first_name, d.last_name
 ORDER BY d.id
