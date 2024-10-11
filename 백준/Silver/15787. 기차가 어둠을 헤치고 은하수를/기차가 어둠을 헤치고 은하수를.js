589f871280ea01439344b44ec53b9f1efe6509f7
Minhee Lee <91667853+leeminhee119@users.noreply.github.com>
2024-10-11 09:49:19 +0900

    [Silver II] Title: 기차가 어둠을 헤치고 은하수를, Time: 516 ms, Memory: 79016 KB -BaekjoonHub

--- 백준/Silver/15787. 기차가 어둠을 헤치고 은하수를/기차가 어둠을 헤치고 은하수를.js
+++ 백준/Silver/15787. 기차가 어둠을 헤치고 은하수를/기차가 어둠을 헤치고 은하수를.js
@@ -3,52 +3,50 @@ const readline = require("readline").createInterface({
   output: process.stdout,
 });
 
-let input = [];
+const lines = [];
 
 readline
   .on("line", function (line) {
-    input.push(line);
+    lines.push(line);
   })
   .on("close", function () {
-    solution(input);
+    solution(lines);
     process.exit();
   });
 
-function solution(input) {
-  const [N, M] = input[0].split(" ").map(Number);
-  const trains = Array.from({ length: N }, () => new Array(20).fill(0));
+function solution(lines) {
+  const [N, M] = lines[0].split(" ").map(Number);
+  const commands = lines.slice(1).map((line) => line.split(" ").map(Number));
+  const trains = Array.from({ length: N }, () => new Array(20 + 1).fill(0));
 
-  function command_execute(command, train, seatNumber) {
-    switch (command) {
+  function executeCommand(commandNo, trainNo, seatNo) {
+    const train = trains[trainNo - 1];
+    switch (commandNo) {
       case 1:
-        train[seatNumber - 1] = 1;
+        train[seatNo] = 1;
         break;
       case 2:
-        train[seatNumber - 1] = 0;
+        train[seatNo] = 0;
         break;
       case 3:
-        for (let i = train.length - 1; i > 0; i--) {
+        for (let i = 20; i > 1; i--) {
           train[i] = train[i - 1];
         }
-        train[0] = 0;
+        train[1] = 0;
         break;
       case 4:
-        for (let i = 0; i < train.length - 1; i++) {
+        for (let i = 1; i < 20; i++) {
           train[i] = train[i + 1];
         }
-        train[train.length - 1] = 0;
+        train[20] = 0;
         break;
     }
+    return train;
   }
-
-  input.slice(1).forEach((rowString) => {
-    const [command, trainNumber, seatNumber] = rowString.split(" ").map(Number);
-    const newTrain = command_execute(
-      command,
-      trains[trainNumber - 1],
-      seatNumber
-    );
+  commands.forEach((command) => {
+    // 명령 수행
+    const train = executeCommand(command[0], command[1], command[2]);
+    trains[command[1] - 1] = train;
   });
-
-  console.log(new Set(trains.map((arr) => arr.join(""))).size);
+  console.log(new Set(trains.map((train) => train.join(""))).size);
 }
