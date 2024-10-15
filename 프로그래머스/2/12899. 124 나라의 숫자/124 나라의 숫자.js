10368ea573281102e759839b74d95f4a8b1e506c
Minhee Lee <91667853+leeminhee119@users.noreply.github.com>
2024-10-15 19:43:49 +0900

    [level 2] Title: 124 나라의 숫자, Time: 0.06 ms, Memory: 33 MB -BaekjoonHub

--- 프로그래머스/2/12899. 124 나라의 숫자/124 나라의 숫자.js
+++ 프로그래머스/2/12899. 124 나라의 숫자/124 나라의 숫자.js
@@ -1,10 +1,13 @@
 function solution(n) {
-    const NUMBERS = ['1', '2', '4'];
-    let string = '';
+    const numbers = ['1', '2', '4'];
+    let str = '';
+    
     while (n > 0) {
         n--;
-        string += NUMBERS[n % 3];
-        n = parseInt(n / 3);
+        const rem = n % 3;
+        str = numbers[rem] + str;
+        n = Math.floor(n / 3);
     }
-    return Array.from(string).reverse().join('');
+    
+    return str;
 }
--- 프로그래머스/2/12899. 124 나라의 숫자/README.md
+++ 프로그래머스/2/12899. 124 나라의 숫자/README.md
@@ -1,10 +1,10 @@
 # [level 2] 124 나라의 숫자 - 12899 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12899) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12899#) 
 
 ### 성능 요약
 
-메모리: 33.5 MB, 시간: 0.10 ms
+메모리: 33 MB, 시간: 0.06 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 10일 18:27:04
+2024년 10월 15일 19:43:46
 
 ### 문제 설명
 
