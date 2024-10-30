4148bbbfc6ad9e7dc8a0168135321f366b1f3681
Minhee Lee <91667853+leeminhee119@users.noreply.github.com>
2024-10-30 19:36:28 +0900

    [level 2] Title: 땅따먹기, Time: 113.45 ms, Memory: 90.7 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.js
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.js
@@ -1,17 +1,9 @@
 function solution(land) {
-    const rows = land.length;
-    const dp = Array.from({length: rows}, () => new Array(4));
-    for (let i = 0; i < 4; i++) {
-        dp[0][i] = land[0][i];
-    }
-    for (let row = 1; row < rows; row++) {
-        for (let col = 0; col < 4; col++) {
-            const prevMax = dp[row - 1].reduce((acc, cur, i) => {
-                if (i === col) return acc;
-                return cur > acc ? cur : acc;
-            }, -1);
-            dp[row][col] = prevMax + land[row][col];
+    const dp = JSON.parse(JSON.stringify(land));
+    for (let i = 1; i < dp.length; i++) {
+        for (let j = 0; j < 4; j++) {
+            dp[i][j] = Math.max(...dp[i - 1].filter((_, idx) => idx !== j)) + dp[i][j];
         }
     }
-    return dp[rows - 1].reduce((acc, cur) => cur > acc ? cur : acc, dp[rows - 1][0]);
+    return Math.max(...dp[land.length - 1]);
 }
